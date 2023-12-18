475fc90a8a0823dd24ff3d7369aefb8a702dc820
wponc <poncyw@gmail.com>
2023-12-17 18:13:58 -0800

    landing page with tree

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Environment, Lightformer, useGLTF, Center, ContactShadows, Sparkles } from '@react-three/drei'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,23 +12,90 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 function App() {
   return (
-    <Canvas>
-      <Experience />
-    </Canvas>
-  )
-}
-
-function Experience(){
-  return(
     <>
-      <OrbitControls />
-      <ambientLight />
-      <Environment preset="studio" background blur={0.7} />
-      
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
-      </mesh>
+      <Overlay />
+      <Canvas
+        camera={ {
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [ 0, 0, 4.5 ]
+        } }
+        >
+        <Experience />
+      </Canvas>
     </>
   )
 }
+
+function Overlay(){
+  return(
+  <>    
+      <div className="welcome">
+          <h2>hi there</h2>
+          <h2>i'm will, and this is my personal site</h2>
+      </div>
+      <div className="navbar">
+          <div className="nav-item" id="menu-button">
+          <span className ="material-symbols-outlined">
+              blur_on
+          </span>
+              <div className="dropdown-content">
+                  <a href="https://fonts.google.com/icons?selected=Material+Symbols+Outlined:blur_on:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=blur" target="_blank">objectives</a>
+                  <a href="#">experience</a>
+                  <a href="#">personal</a>
+              </div>
+          </div>
+          <div className="nav-item">
+              <a href="#">contact</a>
+          </div>
+      </div>
+  </>
+  )
+}
+
+function Experience()
+{
+    return <>
+        <ContactShadows position-y={-1.4} blur={4} color="black" />
+        <Sparkles
+            size={ 3 }
+            scale={ [ 5, 5, 5 ] }
+            position-y={ 1 }
+            speed={ 0.2 }
+            count={ 50 }
+        />
+        <OrbitControls />
+        <color attach="background" args={['#06140d']} />
+        <Environment resolution={256}>
+            <Lightformer form="ring" color="white" intensity={2} scale={2} position={[0, 4, 3]} />
+            <Lightformer form="ring" color="red" intensity={10} scale={2} position={[-2, 3, 2]}/>
+            <Lightformer form="ring" color="#123524" intensity={10} scale={2} position={[2, 3, 2]}  />
+        </Environment>
+        <Center>
+            <Model />
+        </Center>
+    </>
+}
+
+function Model(props) {
+  const group = useRef()
+  const { nodes } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf')
+  useFrame((state, delta) => {
+      group.current.rotation.y += 0.001
+  })
+  return (
+      <group ref={group} {...props} dispose={null} scale={0.1} >
+          <mesh geometry={nodes['tree-spruce'].geometry}>
+              <meshPhysicalMaterial
+                  color={"white"}
+                  clearcoatRoughness={0}
+                  clearcoat={1.0}
+                  roughness={0.2}
+                  metalness={0.9}
+              />
+          </mesh>           
+      </group>
+  )
+}
+useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf')
