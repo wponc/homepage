e1d3cb7f56d192648b1fd36e20c341d8a0166ae7
wponc <poncyw@gmail.com>
2023-12-27 14:13:37 -0800

    transmission homepage

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Environment, Lightformer, useGLTF, Center, ContactShadows, Sparkles } from '@react-three/drei'
+import { OrbitControls, Environment, Text, useGLTF, Center, Lightformer, MeshTransmissionMaterial  } from '@react-three/drei'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -31,12 +31,12 @@ function App() {
 function Overlay(){
   return(
   <>    
-      <div className="welcome">
-          <h2>hi there</h2>
-          <h2>i'm will, and this is my personal site</h2>
-      </div>
-      <div className="navbar">
-          <div className="nav-item" id="menu-button">
+    <div className="welcome">
+        <h2>hi there</h2>
+        <h2>i'm will, and this is my personal site</h2>
+    </div>
+    <div className="navbar">
+      <div className="nav-item" id="menu-button">
           <span className ="material-symbols-outlined">
               blur_on
           </span>
@@ -44,11 +44,11 @@ function Overlay(){
                   <a href="https://experience.will.limited">experience</a>
                   <a href="https://projects.will.limited">projects</a>
                   <a href="https://about.will.limited">personal</a>
-              </div>
           </div>
-          <div className="nav-item">
-              <a href="mailto:willdotlimited@gmail.com">contact</a>
-          </div>
+        </div>
+        <div className="nav-item">
+            <a href="mailto:willdotlimited@gmail.com">contact</a>
+        </div>
       </div>
   </>
   )
@@ -56,46 +56,55 @@ function Overlay(){
 
 function Experience()
 {
-    return <>
-        <ContactShadows position-y={-1.4} blur={4} color="black" />
-        <Sparkles
-            size={ 3 }
-            scale={ [ 5, 5, 5 ] }
-            position-y={ 1 }
-            speed={ 0.2 }
-            count={ 50 }
-        />
+    return(
+    <>
+
         <OrbitControls />
-        <color attach="background" args={['#06140d']} />
-        <Environment resolution={256}>
-            <Lightformer form="ring" color="white" intensity={2} scale={2} position={[0, 4, 3]} />
-            <Lightformer form="ring" color="red" intensity={10} scale={2} position={[-2, 3, 2]}/>
-            <Lightformer form="ring" color="#123524" intensity={10} scale={2} position={[2, 3, 2]}  />
-        </Environment>
+        {/* <Environment>
+          <Lightformer
+            form="rect" // circle | ring | rect (optional, default = rect)
+            intensity={10} // power level (optional = 1)
+            color="blue" // (optional = white)
+            scale={[10, 5]} // Scale it any way you prefer (optional = [1, 1])
+            target={[0, 0, 0]} // Target position (optional = undefined)
+          />
+        </Environment> */}
+        {/* <Environment preset="studio" /> */}
+        <ambientLight />
+
+        <directionalLight position={[0, 3, 0]} />
+
         <Center>
             <Model />
+            <group position={[0, 0, -.5]}>
+              <Text color="#bdb597">
+                will.limited
+              </Text>
+            </group>
         </Center>
     </>
+    )
 }
 
-function Model(props) {
-  const group = useRef()
-  const { nodes } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf')
-  useFrame((state, delta) => {
-      group.current.rotation.y += 0.001
-  })
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/checkeredPlane.glb");
+
   return (
-      <group ref={group} {...props} dispose={null} scale={0.1} >
-          <mesh geometry={nodes['tree-spruce'].geometry}>
-              <meshPhysicalMaterial
-                  color={"white"}
-                  clearcoatRoughness={0}
-                  clearcoat={1.0}
-                  roughness={0.2}
-                  metalness={0.9}
-              />
-          </mesh>           
-      </group>
-  )
+    <group {...props} dispose={null}>
+      <mesh
+        geometry={nodes.Plane.geometry}
+        rotation={[0, -Math.PI / 2, -Math.PI / 2]}
+        scale={[1, 1, 2.5 ]}
+      >
+        <MeshTransmissionMaterial
+          thickness={.5}
+          chromaticAberration={.3}
+          clearcoat={1}
+          color="white"
+        />
+      </mesh>
+    </group>
+  );
 }
-useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf')
+useGLTF.preload("/checkeredPlane.glb");
